b83eaace28c08aed6d827eb7fab1ee2a99929cf9
Joshua Sigston <joshua.sigston@outlook.com>
2023-11-26 21:21:40 -0700

    trying to fix issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,26 @@
 import { useState } from 'react';
 import './App.css';
 // router
-// import { Routes, Route, useLocation } from 'react-router-dom';
-// // components
-// import Navigation from './components/navigation/Navigation';
-// import Hamburger from './components/hamburger/Hamburger';
-// // framer
-// import { AnimatePresence } from 'framer-motion';
-// // pages
-// import { HomePage, AboutPage, ContactPage } from './pages/index';
+import { Routes, Route, useLocation } from 'react-router-dom';
+// components
+import Navigation from './components/navigation/Navigation';
+import Hamburger from './components/hamburger/Hamburger';
+// framer
+import { AnimatePresence } from 'framer-motion';
+// pages
+import { HomePage, AboutPage, ContactPage } from './pages/index';
 
 function App() {
-  // const location = useLocation();
-  // const [nav, setNav] = useState(false);
+  const location = useLocation();
+  const [nav, setNav] = useState(false);
 
-  // const toggleNav = () => {
-  //   setNav(!nav);
-  // };
+  const toggleNav = () => {
+    setNav(!nav);
+  };
 
   return (
     <>
-      <h1>Test</h1>
-      {/* <Navigation nav={nav} toggleNav={toggleNav} />
+      <Navigation nav={nav} toggleNav={toggleNav} />
       <Hamburger nav={nav} toggleNav={toggleNav} />
       <AnimatePresence mode="wait">
         <Routes location={location} key={location.pathname}>
@@ -29,7 +28,7 @@ function App() {
           <Route path="/about" element={<AboutPage />} />
           <Route path="/contact" element={<ContactPage />} />
         </Routes>
-      </AnimatePresence> */}
+      </AnimatePresence>
     </>
   );
 }
