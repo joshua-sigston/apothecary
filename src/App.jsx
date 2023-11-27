cc075c4134213d7a65344b63469d0fff436217ce
Joshua Sigston <joshua.sigston@outlook.com>
2023-11-26 21:14:25 -0700

    testing app on gh pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import { useState } from 'react';
 import './App.css';
 // router
-import { Routes, Route, useLocation } from 'react-router-dom';
-// components
-import Navigation from './components/navigation/Navigation';
-import Hamburger from './components/hamburger/Hamburger';
-// framer
-import { AnimatePresence } from 'framer-motion';
-// pages
-import { HomePage, AboutPage, ContactPage } from './pages/index';
+// import { Routes, Route, useLocation } from 'react-router-dom';
+// // components
+// import Navigation from './components/navigation/Navigation';
+// import Hamburger from './components/hamburger/Hamburger';
+// // framer
+// import { AnimatePresence } from 'framer-motion';
+// // pages
+// import { HomePage, AboutPage, ContactPage } from './pages/index';
 
 function App() {
   const location = useLocation();
@@ -20,7 +20,8 @@ function App() {
 
   return (
     <>
-      <Navigation nav={nav} toggleNav={toggleNav} />
+      <h1>Test</h1>
+      {/* <Navigation nav={nav} toggleNav={toggleNav} />
       <Hamburger nav={nav} toggleNav={toggleNav} />
       <AnimatePresence mode="wait">
         <Routes location={location} key={location.pathname}>
@@ -28,7 +29,7 @@ function App() {
           <Route path="/about" element={<AboutPage />} />
           <Route path="/contact" element={<ContactPage />} />
         </Routes>
-      </AnimatePresence>
+      </AnimatePresence> */}
     </>
   );
 }
